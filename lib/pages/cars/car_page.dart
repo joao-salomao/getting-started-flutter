132c971fcea7193d62415d96653727443ce89449
João Salomão <joaosalomao.613@gmail.com>
2020-01-19 22:59:13 -0300

    Finalizando layout da página do carro

--- lib/pages/cars/car_page.dart
+++ lib/pages/cars/car_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:getting_started/entities/car.dart';
 
 class CarPage extends StatefulWidget {
+  static String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam illud quidem adduci vix possum, ut ea, quae senserit ille, tibi non vera videantur. Quamvis enim depravatae non sint, pravae tamen esse possunt. Minime vero istorum quidem, inquit. Sic enim censent, oportunitatis esse beate vivere. Sin est etiam corpus, ista explanatio naturae nempe hoc effecerit, ut ea, quae ante explanationem tenebamus, relinquamus. Duo Reges: constructio interrete. Esse enim quam vellet iniquus iustus poterat inpune. Quorum altera prosunt, nocent altera. Aperiendum est igitur, quid sit voluptas;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam illud quidem adduci vix possum, ut ea, quae senserit ille, tibi non vera videantur. Quamvis enim depravatae non sint, pravae tamen esse possunt. Minime vero istorum quidem, inquit. Sic enim censent, oportunitatis esse beate vivere. Sin est etiam corpus, ista explanatio naturae nempe hoc effecerit, ut ea, quae ante explanationem tenebamus, relinquamus. Duo Reges: constructio interrete. Esse enim quam vellet iniquus iustus poterat inpune. Quorum altera prosunt, nocent altera. Aperiendum est igitur, quid sit voluptas;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam illud quidem adduci vix possum, ut ea, quae senserit ille, tibi non vera videantur. Quamvis enim depravatae non sint, pravae tamen esse possunt. Minime vero istorum quidem, inquit. Sic enim censent, oportunitatis esse beate vivere. Sin est etiam corpus, ista explanatio naturae nempe hoc effecerit, ut ea, quae ante explanationem tenebamus, relinquamus. Duo Reges: constructio interrete. Esse enim quam vellet iniquus iustus poterat inpune. Quorum altera prosunt, nocent altera. Aperiendum est igitur, quid sit voluptas;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam illud quidem adduci vix possum, ut ea, quae senserit ille, tibi non vera videantur. Quamvis enim depravatae non sint, pravae tamen esse possunt. Minime vero istorum quidem, inquit. Sic enim censent, oportunitatis esse beate vivere. Sin est etiam corpus, ista explanatio naturae nempe hoc effecerit, ut ea, quae ante explanationem tenebamus, relinquamus. Duo Reges: constructio interrete. Esse enim quam vellet iniquus iustus poterat inpune. Quorum altera prosunt, nocent altera. Aperiendum est igitur, quid sit voluptas;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam illud quidem adduci vix possum, ut ea, quae senserit ille, tibi non vera videantur. Quamvis enim depravatae non sint, pravae tamen esse possunt. Minime vero istorum quidem, inquit. Sic enim censent, oportunitatis esse beate vivere. Sin est etiam corpus, ista explanatio naturae nempe hoc effecerit, ut ea, quae ante explanationem tenebamus, relinquamus. Duo Reges: constructio interrete. Esse enim quam vellet iniquus iustus poterat inpune. Quorum altera prosunt, nocent altera. Aperiendum est igitur, quid sit voluptas;";
   Car car;
 
   CarPage(this.car);
@@ -12,6 +13,7 @@ class CarPage extends StatefulWidget {
 
 class _CarPageState extends State<CarPage> {
   Car get car => widget.car;
+  String get loremIpsum => CarPage.loremIpsum;
 
   @override
   Widget build(BuildContext context) {
@@ -49,45 +51,68 @@ class _CarPageState extends State<CarPage> {
       child: ListView(
         children: <Widget>[
           Image.network(car.urlFoto),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    car.nome,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    car.tipo,
-                    style: TextStyle(fontSize: 16),
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                      size: 35,
-                    ),
-                    onPressed: _onClickFavorite,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.share,
-                      size: 35,
-                    ),
-                    onPressed: _onClickShare,
-                  ),
-                ],
-              )
-            ],
-          ),
+          _carHeader(),
+          _carBody(),
         ],
       ),
+    );
+  }
+
+  Row _carHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[_text(car.nome, bold: true), _text(car.tipo)],
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 35,
+              ),
+              onPressed: _onClickFavorite,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 35,
+              ),
+              onPressed: _onClickShare,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _carBody() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 15,),
+          _text("Descrição", bold: true),
+          _text(
+              loremIpsum,
+              justify: true,
+          )
+        ],
+      ),
+    );
+  }
+
+  Text _text(String text, {bool bold = false, bool justify = false}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+      ),
+      textAlign: justify ? TextAlign.justify : TextAlign.left,
     );
   }
 
