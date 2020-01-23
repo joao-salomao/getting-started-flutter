d56d51aa0810da320a9769d656452840b9ba50dc
João Salomão <joaosalomao.613@gmail.com>
2020-01-23 19:55:47 -0300

    Adicionando pacote connectivity para verificar se o dispositivo está conectado a internet

--- lib/pages/cars/cars_bloc.dart
+++ lib/pages/cars/cars_bloc.dart
@@ -1,11 +1,21 @@
+import 'package:getting_started/dao/car_dao.dart';
 import 'package:getting_started/utils/api.dart';
 import 'package:getting_started/entities/car.dart';
+import 'package:getting_started/utils/network.dart';
 import 'package:getting_started/utils/simple_bloc.dart';
 
 class CarsBloc extends SimpleBloc<List<Car>> {
   Future<List<Car>> loadCars(String carsType) async {
     List<Car> cars = [];
     try {
+      bool networkOn = await isNetworkOn();
+      
+      if (!networkOn) {
+        List<Car> cars = await CarDAO().findAllByType(carsType);
+        add(cars);
+        return cars;
+      }
+
       cars = await Api.getCars(carsType);
       add(cars);
     } catch (error) {
